5d96f248c24d701e5a243b4253b286c72699c066
Ivar Nakken <ivarnakken1441@gmail.com>
2021-11-11 19:56:06 +0100

    Finish first task

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cypress-course
+# A course I had on Cypress with Itera
+
+Open the Cypress test runner by typing the following in the terminal
+```bash
+$ yarn cypress open 
+```
--- cypress/integration/tasks/tasks1.js
+++ cypress/integration/tasks/tasks1.js
@@ -3,6 +3,31 @@
 describe('demo', () => {
   it('test search bar', () => {
       cy.visit('https://jira-clone.mad.itera.no/project/board');
-      cy.get('[data-testid="icon:search"]').parent().get('input').type('hello world');
+
+      cy.get('[data-testid="icon:plus"]').click(); 
+      
+      cy.get('[data-testid="modal:issue-create"]').within(() => {
+        
+        cy.get('input[name="title"]').type('a suiting title');
+        
+        cy.get('[data-testid="form-field:description"]').type('a sick description');
+
+        cy.get('[data-testid="select:userIds"]').click() 
+          .parent() 
+          .within(() => {
+            cy.contains('Pickle Rick').click();
+          }); 
+
+        cy.get('[data-testid="form-field:priority"]').click()
+          .within(() => {
+            cy.get('input[placeholder="Search"]').type('Highest'); // This is not actually needed ...
+            cy.contains('Highest').click();  
+          });
+        
+        cy.contains('Create Issue').click();         
+      });  
+    
+    cy.contains('a suiting title').should('be.visible');
+  
   })  
 })
